47ab49e400629475480433c9fd4336b51f39b5ea
angeldrop <fffdan111@163.com>
2020-05-25 17:54:26 +0800

    成功关联了url和view

--- brach_lists/tests.py
+++ brach_lists/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import resolve
+from brach_lists.views import home_page
 
 # Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_打开首页并解析(self):
+        found=resolve('/')
+        self.assertEqual(found.func,home_page)
