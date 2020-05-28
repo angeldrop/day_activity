7540d68521ca7951325400016337dc5f304115e6
angeldrop <fffdan111@163.com>
2020-05-28 11:06:46 +0800

    更改数据库为postgresql

--- day_activity/settings.py
+++ day_activity/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'day_activity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'day_activity_dd',
+        'USER': 'postgres',
+        'PASSWORD': 'Yl;123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
