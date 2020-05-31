c907fc949cec5a8af12dede385add06ad1139ddb
angeldrop <fffdan111@163.com>
2020-05-31 23:42:26 +0800

    添加用户信息到数据库

--- brach_lists/models.py
+++ brach_lists/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class DayActivityUserList(models.Model):
+    id=models.TextField(default='',primary_key=True )
+    order_number=models.TextField(default='')
+    brach_type=models.TextField(default='')
+    full_name=models.TextField(default='')
+    
+
 class Item(models.Model):
     text=models.TextField(default='')
+    list=models.ForeignKey(DayActivityUserList,default=None,on_delete=models.CASCADE)
