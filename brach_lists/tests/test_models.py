943fd3bf73ef851bb0d596d14657fb6ddd92e237
angeldrop <fffdan111@163.com>
2020-07-24 16:53:14 +0800

    拆分单元测试

---
+++ brach_lists/tests/test_models.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+
+from brach_lists.models import Item,DayActivityUserList
+
+# Create your tests here.
+
+class ItemModelTest(TestCase):
+    def test_保存并提取Item数据库项目(self):
+        list_ = DayActivityUserList.objects.create()
+        first_item=Item()
+        first_item.text='The first (ever) list item'
+        first_item.list=list_
+        first_item.save()
+        
+        second_item=Item()
+        second_item.text='Item the second'
+        second_item.list=list_
+        second_item.save()
+        
+        saved_items=Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+        first_saved_item=saved_items[0]
+        second_saved_item=saved_items[1]
+        self.assertEqual(first_saved_item.text,'The first (ever) list item')
+        self.assertEqual(second_saved_item.text,'Item the second')
+    def test_保存并提取DayActivityUserList数据库项目(self):
+        list_ = DayActivityUserList.objects.create(id='ylxxkj',order_number=13,
+            brach_type='部门',full_name='信息科技部')
+        saved_userlist=DayActivityUserList.objects.all()
+        self.assertEqual(saved_userlist.count(),1)
+        self.assertEqual(saved_userlist[0].full_name,'信息科技部')
+
